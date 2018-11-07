c33b8876d80c3a7f802bd325643e0b28409f621d
Natalie Halawani <hala1224@gmail.com>
2018-11-07 23:53:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function produceDrivingRange(blockRange) {
   return function(blocks) {
     if (blocks-blockRange > 0) {
-      return `within range by $(blocks-blockRange)`;
+      return `within range by {blocks-blockRange}`;
     } else {
-      return '$Math.abs(blocks-blockRange) blocks out of range';
+      
+      return '${Math.abs(blocks-blockRange)} blocks out of range';
     }
   };
 }
